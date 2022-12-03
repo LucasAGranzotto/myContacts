5797356046f94d9691d1ca2f1b84375727633de0
Lucas Alexandre Granzotto <lucasagranzotto@gmail.com>
2022-12-02 23:34:43 -0300

    Implemented Category Controller and Repository

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -1,37 +1,103 @@
 const { StatusCodes } = require('http-status-codes');
-const CategoryRepository = require('../repositories/CategoryRepository');
+const CategoriesRepository = require('../repositories/CategoriesRepository');
 
 class CategoryController {
   async #getCategoryById(id) {
-    // TO-DO
+    const contact = await CategoriesRepository.findById(id);
+    return contact;
   }
 
   async #getCategoryByName(name) {
-    // TO-DO
+    const contact = await CategoriesRepository.findByName(name);
+    return contact;
   }
 
   #categoryNotExists(res, id) {
-    // TO-DO
+    res.status(StatusCodes.NOT_FOUND).json({
+      errors: [
+        {
+          value: id,
+          msg: 'Category not found',
+        },
+      ],
+    });
+  }
+
+  #categoryAlreadyExists(res, name) {
+    res.status(StatusCodes.BAD_REQUEST).json({
+      errors: [
+        {
+          value: name,
+          msg: 'This category already exists',
+        },
+      ],
+    });
   }
 
   async index(req, res) {
-    // TO-DO
+    const { orderBy } = req.query;
+    const contacts = await CategoriesRepository.findAll(orderBy);
+    res.json(contacts);
   }
 
   async show(req, res) {
-    // TO-DO
+    // listar um registro
+    const { id } = req.params;
+
+    const contact = await this.#getCategoryById(id, res);
+
+    if (!contact) {
+      return this.#categoryNotExists(res, id);
+    }
+
+    res.json(contact);
   }
 
   async store(req, res) {
-    // TO-DO
+    // criar um registro
+    const { name } = req.body;
+
+    const contact = await this.#getCategoryByName(name);
+
+    if (contact) {
+      return this.#categoryAlreadyExists(res, name);
+    }
+
+    const newContact = await CategoriesRepository.create({ name });
+
+    res.json(newContact);
   }
 
   async update(req, res) {
-    // TO-DO
+    // atualizar um registro
+    const { id } = req.params;
+
+    const { name } = req.body;
+
+    const categoryById = await this.#getCategoryById(id, res);
+    const categoryByName = await this.#getCategoryByName(name, res);
+
+    if (!categoryById) {
+      return this.#categoryNotExists(res, id);
+    }
+
+    if (categoryByName) {
+      if (categoryById.id !== categoryByName.id) {
+        return this.#categoryAlreadyExists(res, name);
+      }
+    }
+
+    const category = await CategoriesRepository.update(id, { name });
+
+    res.json(category);
   }
 
   async delete(req, res) {
-    // TO-DO
+    // excluir um registro
+    const { id } = req.params;
+
+    await CategoriesRepository.delete(id);
+    res.sendStatus(StatusCodes.NO_CONTENT);
   }
 }
 
--- src/app/repositories/CategoriesRepository.js
+++ src/app/repositories/CategoriesRepository.js
@@ -2,27 +2,76 @@ const db = require('../../database');
 
 class CategoriesRepository {
   async findAll(orderBy = 'ASC') {
-    // TO-DO
+    const direction = orderBy.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
+    const rows = await db.query(
+      `
+        SELECT *
+          FROM categories
+         ORDER BY name ${direction}
+      `,
+    );
+    return rows;
   }
 
   async findById(id) {
-    // TO-DO
+    const [row] = await db.query(
+      `
+          SELECT *
+            FROM categories
+           WHERE id = $1
+        `,
+      [id],
+    );
+
+    return row;
   }
 
   async findByName(name) {
-    // TO-DO
+    const [row] = await db.query(
+      `
+        SELECT *
+          FROM categories
+         WHERE name = $1
+      `,
+      [name],
+    );
+    return row;
   }
 
   async create({ name }) {
-    // TO-DO
+    const [row] = await db.query(
+      `
+        INSERT INTO categories(name)
+        VALUES($1)
+        RETURNING *
+      `,
+      [name],
+    );
+    return row;
   }
 
   async update(id, { name }) {
-    // TO-DO
+    const [row] = await db.query(
+      `
+        UPDATE categories
+           SET name = $1
+         WHERE id = $2
+        RETURNING *
+      `,
+      [name, id],
+    );
+    return row;
   }
 
   async delete(id) {
-    // TO-DO
+    const deleteOp = await db.query(
+      `
+        DELETE FROM categories
+         WHERE id = $1
+      `,
+      [id],
+    );
+    return deleteOp;
   }
 }
 
